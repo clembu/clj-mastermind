4509ded0c36e214346db9e98903fe1216fb9e741
Clément Busschaert <clement.busschaert@outlook.com>
2017-01-31 11:47:34 +0100

    Working Plus/Minus game
    Example for input/output loop

--- src/mastermind/core.clj
+++ src/mastermind/core.clj
@@ -1,7 +1,40 @@
 (ns mastermind.core
   (:gen-class))
 
+(defn answer [ans guess]
+  (if (and (number? ans) (number? guess))
+    (cond
+      (= ans guess)
+      :win
+      (< ans guess)
+      :high
+      (> ans guess)
+      :low
+      :else
+      :error)
+    :error))
+
+(defn react [ans]
+  (answer ans (int (read))))
+
+(defn play [ans]
+  (loop [res :begin]
+    (cond
+      (= res :error)
+      (println "There's been an error")
+      (= res :win)
+      (println "You guessed right!!")
+      (= res :low)
+      (or (println "Too low :p")
+          (recur (react ans)))
+      (= res :high)
+      (or (println "Too high x)")
+          (recur (react ans)))
+      :else
+      (or (println "Enter a number")
+          (recur (react ans))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (play (rand-int 100)))

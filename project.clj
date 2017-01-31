f327a84084876a55265d55ce0718ea80fc5220e0
Clément Busschaert <clement.busschaert@outlook.com>
2017-01-31 11:46:50 +0100

    Project basics

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mastermind "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A smart mastermind app who tries to guess your code"
+  :url "https://github.com/facelesspanda/clj-mastermind"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
